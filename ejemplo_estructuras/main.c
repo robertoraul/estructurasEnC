fef1fb9dffe6b11afd81d5bdaf82186b17a317b0
Your Name <you@example.com>
2018-04-20 17:31:20 -0300

    primer estructura

---
+++ ejemplo_estructuras/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef{
+    int legajo;
+    int notaUno;
+    int notados;
+    int estados;
+    float promedio;
+    char[50] nombre;
+    int estado;
+}EAlumno;
+
+
+int main()
+{
+
+
+    printf("Hello world!\n");
+    return 0;
+}
